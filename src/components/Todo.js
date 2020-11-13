71d869c22b76b5e2ea8264cf9ca6878fec4ad930
Yousif Abozid <yousif.abozid@yahoo.com>
2020-11-13 04:33:42 +0200

    Adding complete feature

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,12 +5,23 @@ const Todo = ({ todo, todos, setTodos }) => {
         setTodos(todos.filter(object => object.id !== todo.id ))
     }
 
+    const handleComplete = () => {
+        setTodos(todos.map((object) => {
+            if (object.id === todo.id) {
+                return {
+                    ...object, completed: !object.completed
+                }
+            }
+            return object
+        }))
+    }
+
     return (
         <div className="todo">
-            <li className="todo-item">
+            <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
                 {todo.text}
             </li>
-            <button className="complete-btn">
+            <button onClick={handleComplete} className="complete-btn">
                 <i className="fas fa-check"></i>
             </button>
             <button onClick={handleDelete} className="trash-btn">
